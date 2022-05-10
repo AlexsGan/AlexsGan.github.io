ec32fe09e974a61f1d59c920b6b814aa27aa4826
alexgan <alex.gan@mail.utoronto.ca>
2022-05-10 15:36:29 -0400

    modified intro

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,5 +1,5 @@
 import "./intro.css"
-import Me from "../../img/me.jpg"
+import Me from "../../img/me_transparent_1.png"
 
 const Intro = () => {
     return (
@@ -10,25 +10,23 @@ const Intro = () => {
                     <h1 className="i-name">Alex Gan</h1>
                     <div className="i-title">
                         <div className="i-title-wrapper">
-                            <div className="i-title-item">Software Development Engineer</div>
+                            <div className="i-title-item">Software Engineer</div>
                             <div className="i-title-item">Gamer</div>
-                            <div className="i-title-item">Pianist</div>
+                            <div className="i-title-item">Pianist </div>
                             <div className="i-title-item">Swimmer</div>
                         </div>
                     </div>
-                    <svg>
-                        
-                    </svg>
+                    {/* <svg></svg> */}
                     <p className="i-desc">
                     I am a passionate student enrolled in the Computer Science Specialist program at the University of Toronto. I love working in fast-paced environments and I constantly seek opportunities that help me become a better software engineer.
                     </p>
                 </div>
             </div>
             <div className="i-right">
-                <div className="i-right-wrapper">
-                    {/* <div className="i-bg"></div> */}
+                <div className="i-bg"></div>
+                {/* <div className="i-right-wrapper"> */}
                     <img src={Me} alt="" className="i-img" />
-                </div>
+                {/* </div> */}
             </div>
         </div>
     )
