a92feb4f77a8fd2a34460f7d0a52da8ef5d9f766
alexgan <alex.gan@mail.utoronto.ca>
2022-05-19 15:01:19 -0400

    added resume link and minor changes

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,11 +1,13 @@
 import "./about.css"
 import Me from "../../img/me.jpg"
 import React, { Component } from "react"
+import Button from '@mui/material/Button';
 
 class About extends Component {
     render() {
         return (
             <div className="a">
+                {/* <div className="a-container"> */}
                 <div className="a-left">
                     <div className="a-card bg"></div>
                     <div className="a-card">
@@ -14,10 +16,20 @@ class About extends Component {
                 </div>
                 <div className="a-right">
                     <h1 className="a-title">About Me</h1>
-                    <p className="i-desc">
+                    <p className="a-desc">
                         I am a passionate student enrolled in the Computer Science Specialist program at the University of Toronto. I love working in fast-paced environments and I constantly seek opportunities that help me become a better software engineer.
                     </p>
+                    <div className="a-resume">
+                        <Button
+                            variant="contained"
+                            href="https://drive.google.com/file/d/10lzAmH2ayPKe9qvliDhPGBFcmvGHRRlK/view?usp=sharing"
+                            target="_blank"
+                        >
+                            <p>Resume</p>
+                        </Button>
+                    </div>
                 </div>
+                {/* </div> */}
             </div>
         )
     }
