fa2fb5f30560365125568b686e97dec72cd64fd9
alexgan <alex.gan@mail.utoronto.ca>
2022-06-15 16:46:35 -0400

    fixed changing lightmode bg color bug on iphone

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -12,7 +12,7 @@ class Intro extends Component {
             <div className="i">
                 <div className="i-left">
                     <div className="i-left-wrapper">
-                        <h2 className="i-intro">Hello, My name is</h2>
+                        <h2 className="i-intro">Hello! My name is</h2>
                         <h1 className="i-name">Alex Gan</h1>
                         <div className="i-title">
                             <div className="i-title-wrapper">
